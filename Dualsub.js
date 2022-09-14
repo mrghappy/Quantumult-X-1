8bf45e1bd3de2470986a78e345c46e78073be0f3
mrghappy <mrghappy@gmail.com>
2022-09-14 16:48:54 +0200

    Update Dualsub.js

--- Dualsub.js
+++ Dualsub.js
@@ -276,13 +276,14 @@ if (service == "YouTube") {
             timeline = body.match(/<p t="\d+" d="\d+"[^>]+>/g)
         }
 
-          for (var i in timeline) {
+        for (var i in timeline) {
             let patt = new RegExp(`${timeline[i]}([^<]+)<\\/p>`)
             if (body.match(patt) && response.body.match(patt)) {
-                if (setting.line == "s") body = body.replace(patt, `${timeline[i].replace(/\n/g, " ")}$1 \n${response.body.match(patt)[1]}</p>`)
-                if (setting.line == "f") body = body.replace(patt, `${timeline[i].replace(/\n/g, " "}${response.body.match(patt)[1]} =\n$1</p>`)
+                if (setting.line == "s") body = body.replace(patt, `${timeline[i]}$1\n${response.body.match(patt)[1]}</p>`)
+                if (setting.line == "f") body = body.replace(patt, `${timeline[i]}${response.body.match(patt)[1]}\n$1</p>`)
             }
         }
+
 
 
         $done({ body })
